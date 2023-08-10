1c475ea3475f483dbe764ef7c0f4838f67a0b8f0
chatinunk97 <chatinun.hobby@gmail.com>
2023-08-10 08:45:46 +0700

    Optimize coding patterns

--- 02-do-while/01-findAverage/main.js
+++ 02-do-while/01-findAverage/main.js
@@ -2,7 +2,6 @@ let counter = 0 ;
 let sum_text = ""
 let sum = 0;
 
-let isNull
 
 do{
     
@@ -11,12 +10,13 @@ do{
     // Set condition to check
     isEmpty = number.trim() == "";
     isNan = isNaN(number);
+    isNegZero = number <= 0;
 
-    if (!isEmpty && !isNan){
+    if (!isEmpty && !isNan && !isNegZero){
         sum += +number
         sum_text += `${number} `
         counter +=1
     }
 
-}while(!isEmpty && !isNull && !isNan)
+}while(!isEmpty &&  !isNan && !isNegZero)
 console.log(`You have input the following number ${sum_text} ${"\n"} and your average value is ${(sum / counter).toFixed(2)}` )
